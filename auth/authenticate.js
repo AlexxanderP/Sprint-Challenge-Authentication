74171a225de9c908c76837d1f2c01e42ca06283e
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-04-05 10:09:30 -0700

    Routes turned back to functions, test user posted to register via postman

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,9 +1,6 @@
 const jwt = require("jsonwebtoken");
 const db = require("../database/dbConfig.js");
-
-const jwtKey =
-  process.env.JWT_SECRET ||
-  "add a .env file to root of project with the JWT_SECRET variable";
+const jwtKey = require("../auth/secrets").jwtKey;
 
 // quickly see what this file exports
 module.exports = {
--- config/routes.js
+++ config/routes.js
@@ -14,7 +14,21 @@ module.exports = server => {
 };
 
 //----- Post Router for Register -----//
-router.post("/register", (req, res) => {
+// router.post("/register", (req, res) => {
+//   let user = req.body;
+//   const hash = bcrypt.hashSync(user.password, 10);
+//   user.password = hash;
+
+//   Users.add(user)
+//     .then(saved => {
+//       res.status(201).json(saved);
+//     })
+//     .catch(error => {
+//       res.status(500).json(error);
+//     });
+// });
+
+function register(req, res) {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
@@ -26,10 +40,32 @@ router.post("/register", (req, res) => {
     .catch(error => {
       res.status(500).json(error);
     });
-});
+}
 
 //----- Post Router for Login ------//
-router.post("/login", (req, res) => {
+// router.post("/login", (req, res) => {
+//   let { username, password } = req.body;
+
+//   Users.findBy({ username })
+//     .first()
+//     .then(user => {
+//       if (user && bcrypt.compareSync(password, user.password)) {
+//         const token = generateToken(user);
+
+//         res.status(200).json({
+//           message: `Welcome ${user.username}! You now have a token!`,
+//           token
+//         });
+//       } else {
+//         res.status(401).json({ message: "Invalid credentials" });
+//       }
+//     })
+//     .catch(error => {
+//       res.status(500).json(error);
+//     });
+// });
+
+function login(req, res) {
   let { username, password } = req.body;
 
   Users.findBy({ username })
@@ -49,8 +85,7 @@ router.post("/login", (req, res) => {
     .catch(error => {
       res.status(500).json(error);
     });
-});
-
+}
 //----- Get Jokes Function -----//
 function getJokes(req, res) {
   const requestOptions = {
