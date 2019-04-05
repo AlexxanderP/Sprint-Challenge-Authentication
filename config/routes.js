8bcfada02cfc8a1f46052420c74f5b77be3df841
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-04-05 09:56:18 -0700

    post router for register and login created, generatetoken function created

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,11 @@
 const axios = require("axios");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+
 const { authenticate } = require("../auth/authenticate");
+
 const secret = require("../auth/authenticate").jwtKey;
+const Users = require("../auth/authenticate");
 
 module.exports = server => {
   server.post("/api/register", register);
@@ -10,14 +13,45 @@ module.exports = server => {
   server.get("/api/jokes", authenticate, getJokes);
 };
 
-function register(req, res) {
-  // implement user registration
-}
+//----- Post Router for Register -----//
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
 
-function login(req, res) {
-  // implement user login
-}
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
+//----- Post Router for Login ------//
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.username}! You now have a token!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+//----- Get Jokes Function -----//
 function getJokes(req, res) {
   const requestOptions = {
     headers: { accept: "application/json" }
@@ -31,4 +65,18 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
+}
+
+//---- Generate Token Function -----//
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secret, options);
 }
